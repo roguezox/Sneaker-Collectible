450043bb3c40be3255ffff433f7330ad7b61b7f3
roguezox <ak9001099436@gmail.com>
2025-12-05 14:47:06 +0530

    fixed asset render issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig(() => ({
     plugins: [react()],
-    publicDir: "./static",
+    publicDir: "./public",
     base: "./",
     css: {
         postcss: {
